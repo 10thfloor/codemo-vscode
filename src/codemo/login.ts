5620e960ade91c61a5c3b93903f88c6c104cc908
10thfloor <remember.mackenzie@gmail.com>
2017-03-16 10:36:14 -0700

    Login page can talk to firebase.

--- src/codemo/login.ts
+++ src/codemo/login.ts
@@ -18,15 +18,39 @@ class TextDocumentContentProvider implements vscode.TextDocumentContentProvider 
 
 	private page(): string {
 		return `<style>
+					body, html {
+						display:flex;
+						flex-direction: column;
+						width: 100%;
+						height: 100%;
+						background: #222;
+						align-items: center;
+						justify-content: center;
+					}
 				</style>
 				<body>
-					<script>
-					<script>
+					Hello?
 					<form>
 						<input type="text" placeholder="username"/>
-						<input type="password" placeholder="username"/>
+						<input type="password" placeholder="password"/>
 						<button type="button" id="login">Login</button>
 					<form>
+					<script src="https://www.gstatic.com/firebasejs/3.7.1/firebase.js"></script>
+					<a class="jsbin-embed" href="https://jsbin.com/ciwenojode/1/embed?output"></a><script src="https://static.jsbin.com/js/embed.min.js?3.41.6"></script>
+					<script>
+					const config = {
+						apiKey: "AIzaSyCnJQepRSQ9A1yAannhT76ugtUGGyrEU2s",
+						authDomain: "codemo-21829.firebaseapp.com",
+						databaseURL: "https://codemo-21829.firebaseio.com",
+						storageBucket: "codemo-21829.appspot.com",
+						messagingSenderId: "1095893987589"
+					};
+					firebase.initializeApp(config);
+					var button = document.getElementById('login');
+						button.addEventListener('click', function() {
+							console.log(firebase);
+						});
+					</script>
 				</body>`;
 	}
 }
